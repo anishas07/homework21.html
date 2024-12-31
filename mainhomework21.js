7f336be7badf103629978422ed77855cf0ef9801
anishas07 <Studytea95@gmail.com>
2024-12-31 12:24:17 +1030

    Add files via upload

--- mainhomework21.js
+++ mainhomework21.js
@@ -1,13 +1,13 @@
 function display() 
 { 
-    var monthIndex = mydata.getMonth(); 
-    var dayIndex = mydata.getDay(); 
+    var month = mydata.getMonth(); 
+    var day = mydata.getDay(); 
     var currentDate = mydata.getDate(); 
 
 var month = ["Decemeber", "January", "February"]  
     var date = ["Monday", "Friday", "Wednesday"];
 
-    document.getElementById("month1").innerText = "Month: " + months[monthIndex];
+    document.getElementById("month1").innerText = "Month: " + months[month];
     document.getElementById("date1").innerText = "Date: " + currentDate;
-    document.getElementById("day1").innerText = "Day: " + days[dayIndex];
+    document.getElementById("day1").innerText = "Day: " + days[day];
 }
